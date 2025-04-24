fedf516fd5c1ada15c721ba1c34a40e87b10d4bf
Josiah Elias <josiahelias11@gmail.com>
2025-04-24 20:50:04 +0100

    feat: function to add transaction created

---
+++ src/controllers/transactionControllers.ts
@@ -0,0 +1,29 @@
+import { transactionServices } from "../services/transactionServices";
+import { Request, Response } from "express";
+
+interface TransactionData {
+  item_name: string;
+  quantity: number;
+  amount: number;
+}
+
+export const addTransaction = async (req: Request, res: Response) => {
+  const transactionData: TransactionData = req.body;
+  const { user_id } = req.params;
+
+  try {
+    const response = await transactionServices.addTransaction({
+      ...transactionData,
+      user_id,
+    });
+
+    if (response.error) {
+      res.status(500).json({ error: "Failed to add transaction" });
+    }
+
+    res.status(201).json({ success: "Transaction added", data: response.data });
+  } catch (error) {
+    console.log("Error in delete transaction service", error);
+    res.status(500).json({ error: "Failed to add transaction" });
+  }
+};
