659a11cf3ffe8c6f002313b4de2ff7cefbf44dd6
Josiah Elias <josiahelias11@gmail.com>
2025-05-07 12:25:14 +0100

    feat: tests to verify pin done

--- src/tests/utils/pin.test.ts
+++ src/tests/utils/pin.test.ts
@@ -3,29 +3,29 @@ import argon2 from "argon2";
 
 jest.mock("argon2");
 
-// describe("hash pin", () => {
-//   it("should return a hashed pin", async () => {
-//     const pin = "1234";
-//     const mockedPin = "hashedPin1234";
+describe("hash pin", () => {
+  it("should return a hashed pin", async () => {
+    const pin = "1234";
+    const mockedPin = "hashedPin1234";
 
-//     argon2.hash = jest.fn().mockResolvedValue(mockedPin);
+    argon2.hash = jest.fn().mockResolvedValue(mockedPin);
 
-//     const result = await pinServices.hashPin(pin);
+    const result = await pinServices.hashPin(pin);
 
-//     expect(result).toBe(mockedPin);
-//     expect(argon2.hash).toHaveBeenCalledWith(pin);
-//   });
+    expect(result).toBe(mockedPin);
+    expect(argon2.hash).toHaveBeenCalledWith(pin);
+  });
 
-//   it("should return error if hashing failed", async () => {
-//     const pin = "1234";
+  it("should return error if hashing failed", async () => {
+    const pin = "1234";
 
-//     argon2.hash = jest.fn().mockRejectedValue(new Error("Hashing failed"));
+    argon2.hash = jest.fn().mockRejectedValue(new Error("Hashing failed"));
 
-//     await expect(pinServices.hashPin(pin)).rejects.toThrow(
-//       "Failed to hash password"
-//     );
-//   });
-// });
+    await expect(pinServices.hashPin(pin)).rejects.toThrow(
+      "Failed to hash password"
+    );
+  });
+});
 
 describe("verify pin", () => {
   it("should verify and return a boolean", async () => {
